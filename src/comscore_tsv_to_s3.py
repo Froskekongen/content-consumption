cbf97e20c9032de75e5f2f69ececad8433267db7
Erlend Aune <erlend.aune@amedia.no>
2015-10-25 09:27:33 +0100

    .

--- src/comscore_tsv_to_s3.py
+++ src/comscore_tsv_to_s3.py
@@ -5,17 +5,29 @@ import sys
 
 fn=sys.argv[3]
 
-#conn=S3Connection(sys.argv[1],sys.argv[2])
-#bucket=conn.get_bucket('s3-acpcontent')
+conn=S3Connection(sys.argv[1],sys.argv[2])
+bucket=conn.get_bucket('s3-comscore-consumption-daily')
 
 with open(fn) as ff:
-    dumpdata=[]
+    key_in_bucket=fn.split('/')[-1]
+    dumpdata=''
     n_lines=0
     lines_per_file=5000000
     for iii,line in enumerate(ff):
         n_lines+=1
-        dumpdata.append(line)
+        dumpdata+=line
         if (n_lines%lines_per_file)==0:
             print(line)
 
+            output=''.join(dumpdata)
             dumpdata.clear()
+            k=Key(bucket)
+            k.key=key_in_bucket+'/'+str(n_lines).zfill(11)+'.tsv'
+            k.set_contents_from_string(output)
+
+    if len(dumpdata)>0:
+        output=''.join(dumpdata)
+        dumpdata.clear()
+        k=Key(bucket)
+        k.key=key_in_bucket+'/'+str(n_lines).zfill(11)+'.tsv'
+        k.set_contents_from_string(output)
